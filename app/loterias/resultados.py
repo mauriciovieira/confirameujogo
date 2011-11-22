dbb3ad0e54636b65cc2a5efd4da353c39ca440a2
Maurício Vieira <mauricio@mauriciovieira.net>
2011-11-22 11:50:19 -0200

    Links dos arquivos zips com os resultados

---
+++ app/loterias/resultados.py
@@ -0,0 +1,18 @@
+"""
+Arquivos de resultados para download.
+"""
+# http://ikaisays.com/2011/02/19/unit-testing-in-tipfy-an-app-engine-framework-in-python/
+
+ZIPS = {
+'megasena': 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip', 
+'quina': 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_quina.zip',
+'duplasena': 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/d_dplsen.zip',
+'federal': 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_federa.zip',
+'loteca': 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/d_loteca.zip',
+'timemania': 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_timema.zip', 
+'lotofacil': 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_lotfac.zip',
+'lotomania': 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_lotoma.zip',
+'lotogol': 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/d_lotogo.zip'
+}
+
+
